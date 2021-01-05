fa062e1090e9ea362332fd4207beff55b5f6bff7
Martin Insulander <martin@BULLSEYE-debian-live-builder>
2021-01-05 08:35:29 +0100

    Updated installation
    Kill old session by checking pid from dbus

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Secret Service runner for KDE Plasma Krunner in python
 
 Run install.sh to install it locally.
+
+
+# Requires
+python3-secretstorage
+
--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
-PLUGIN_PATH="$HOME/.local/share/krunner/plugin/miSecretService/"
+#!/bin/sh
 
-echo "Put plugin in: $PLUGIN_PATH"
-mkdir -p "$PLUGIN_PATH"
-cp secret.py "$PLUGIN_PATH"
-cp clipboard.py "$PLUGIN_PATH"
-cp krunner.py "$PLUGIN_PATH"
-cp miSecretService.py "$PLUGIN_PATH"
-chmod u+x "$PLUGIN_PATH/miSecretService.py"
+CODE_PATH="$HOME/.local/share/krunner/plugin/miSecretService/"
+KRUNNER_DBUSPLUGIN_PATH="$HOME/.local/share/krunner/dbusplugins/"
+
+echo "Putting plugin in: $CODE_PATH"
+mkdir -p "$CODE_PATH"
+cp secret.py "$CODE_PATH"
+cp clipboard.py "$CODE_PATH"
+cp krunner.py "$CODE_PATH"
+cp miSecretService.py "$CODE_PATH"
+chmod u+x "$CODE_PATH/miSecretService.py"
 
 AUTOSTART_PATH="$HOME/.config/autostart/"
 echo "Enable in autostart: $AUTOSTART_PATH"
@@ -14,16 +17,20 @@ mkdir -p "$AUTOSTART_PATH"
 cp miSecretService-autostart.desktop "$AUTOSTART_PATH"
 
 KSERVICE_PATH="$HOME/.local/share/kservices5/"
-echo "Enable in kservice5: $KSERVICE_PATH"
-mkdir -p "$KSERVICE_PATH"
-cp plasma-runner-miSecretService.desktop "$KSERVICE_PATH"
+echo "Enable in krunner: $KRUNNER_DBUSPLUGIN_PATH"
+mkdir -p "$KRUNNER_DBUSPLUGIN_PATH"
+cp plasma-runner-miSecretService.desktop "$KRUNNER_DBUSPLUGIN_PATH"
 
-echo "Stopping old miSecretService (if any)"
-killall miSecretService.py
+echo "Check for old miSecretService process"
+oldpid=$(gdbus call --session --dest org.freedesktop.DBus --object-path / --method org.freedesktop.DBus.GetConnectionUnixProcessID mi.secretservice.krunner | grep -oP " \K\d+") 2>/dev/null
+if [ -n "$oldpid" ]; then
+  echo "Stopping old miSecretService"
+  kill $oldpid
+fi
 
 echo "Launching service miSecretService"
-(cd $PLUGIN_PATH; nohup ./miSecretService.py &)
-echo "(log under $PLUGIN_PATH/nohup.out)"
+(cd $CODE_PATH; nohup ./miSecretService.py &)
+echo "(log under $CODE_PATH/nohup.out)"
 
 echo "Killing krunner to read in new configuration"
 killall krunner  # starts automagically
